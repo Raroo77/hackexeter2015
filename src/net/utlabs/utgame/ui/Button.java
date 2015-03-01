9ee8464744f15e5a1c847f3d917f3d749fe9e988
masinister <mason.dicicco@gmail.com>
2015-03-01 03:32:44 -0500

    Update M:7 +Game UI *Buttons/Components

--- src/net/utlabs/utgame/ui/Button.java
+++ src/net/utlabs/utgame/ui/Button.java
@@ -6,28 +6,46 @@ import org.lwjgl.opengl.GL11;
 
 /**
  * Created by mas.dicicco on 3/1/2015.
+ * Button component for Ui 
  */
 public class Button extends Component {
 
+
+    /**
+     * Currently selected component*
+     */
+    public boolean mSelected;
+    /**
+     * Texture of button because font rendering is too hard*
+     */
     private Texture mTexture;
 
+    /**
+     * Constructs Button
+     *
+     * @param parent Parent UI
+     * @param id     Index
+     * @param x      Topleft x coordinate
+     * @param y      Topleft y coordinate
+     * @param width  width of image
+     * @param height height of image
+     * @param t      Texture
+     */
     public Button(Ui parent, int id, int x, int y, int width, int height, Texture t) {
         super(parent, id, x, y, width, height);
         mTexture = t;
     }
-
+    
     @Override
     public boolean keyEvent(int key, char ch, boolean down, long duration) {
-        if (key == Keyboard.KEY_SPACE && mParent.mCurrentButton == mId) {
+        if (key == Keyboard.KEY_SPACE)
             fire();
-            return true;
-        }
         return false;
     }
 
     @Override
     public void render(int delta) {
-        if (mParent.mCurrentButton == mId) {
+        if (mSelected) {
             GL11.glColor3f(0, 1, 0);
         } else
             GL11.glColor3f(1, 1, 1);
--- src/net/utlabs/utgame/ui/Component.java
+++ src/net/utlabs/utgame/ui/Component.java
@@ -1,12 +1,38 @@
 package net.utlabs.utgame.ui;
 
+/**
+ * Any Component of a Menu
+ * Actually just buttons but whatever
+ */
 public class Component {
 
+    /**
+     * Component's index in the array of components
+     */
     public final int mId;
+    /**
+     * Parent UI*
+     */
     public final Ui mParent;
+    /**
+     * Coordinates of top left corner of component for drawing
+     */
     public int mX, mY;
+    /**
+     * width and height of the components*
+     */
     public int mWidth, mHeight;
 
+    /**
+     * Constructs Component*
+     *
+     * @param parent: parent
+     * @param id:     index
+     * @param x:      x coordinate
+     * @param y:      y coordinate
+     * @param width:  width of component
+     * @param height: height of component
+     */
     public Component(Ui parent, int id, int x, int y, int width, int height) {
         mParent = parent;
         mId = id;
@@ -16,15 +42,36 @@ public class Component {
         mHeight = height;
     }
 
-    public void update(int delta) {}
+    /**
+     * Update, obviously 
+     * @param delta: timescale
+     */
+    public void update(int delta) {
+    }
 
-    public void render(int delta) {}
+    /**
+     * Render
+     * @param delta:timescale
+     */
+    public void render(int delta) {
+    }
 
+    /**
+     * Menu Navigation 
+     * @param key:Key Pressed
+     * @param ch:Character pressed
+     * @param down:Is pressed
+     * @param duration:How long is pressed
+     * @return
+     */
     public boolean keyEvent(int key, char ch, boolean down, long duration) {
 
         return false;
     }
 
+    /**
+     * Press button
+     */
     protected void fire() {
         mParent.childFired(mId);
     }
