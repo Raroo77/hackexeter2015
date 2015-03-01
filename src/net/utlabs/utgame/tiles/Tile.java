91edd3d5ea725564781fa598da8bc69824d707f2
Raroo <rarroo77@gmail.com>
2015-03-01 11:20:07 -0500

    Update R8 + added CollisionHelper (No use yet)

--- src/net/utlabs/utgame/tiles/Tile.java
+++ src/net/utlabs/utgame/tiles/Tile.java
@@ -2,12 +2,14 @@ package net.utlabs.utgame.tiles;
 
 import net.utlabs.utgame.Player;
 import net.utlabs.utgame.Room;
+import net.utlabs.utgame.Vector;
 
 /**
  * Created by mas.dicicco on 2/28/2015
  * Abstract representation of all tiles which can exist in a room
  */
 public abstract class Tile {
+    public static final int SIZE = 60;
 
     /**
      * Assigns tiles integer values so we don't have to initialize like a thousand of them
@@ -53,7 +55,24 @@ public abstract class Tile {
      * @param player
      * @return
      */
+
     public abstract boolean collision(int x, int y,Player player);
+
+    /**
+     *
+     * @param r Takes values from (-pi, pi]
+     * @return
+     */
+//    @Deprecated
+//    public Vector collisionHelper(double r){
+//        if(r>-Math.PI && r < -3*Math.PI/4){
+//            return new Vector((float) -SIZE /2,(float) (-SIZE/2*Math.tan(Math.abs(Math.PI-Math.abs(r)))));
+//        }
+//        if(r>-Math.PI/4 && r<Math.PI/4){
+//            return new Vector((float)SIZE/2, (float) (SIZE/2*Math.tan(r)));
+//        }
+//        if()
+//    }
 
     /**
      * Draws
